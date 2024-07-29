f1de9639b4b5c699383c05fd4ad988c842246ce7
Naveen Kuratti <naveenkuratti875@gmail.com>
2024-07-29 13:45:03 +0530

    Your commit message

---
+++ Hello.c
@@ -0,0 +1,40 @@
+# include<stdio.h>
+
+
+int main(){
+   /* int number =25;
+    char star='*';
+    int age=22;
+    float pi=3.14;
+    return 0;
+    */
+  
+
+  /*printf("Hello C\n");
+    printf("Hello C\n");
+        printf("Hello C\n");
+    */
+   /*char star='*';
+   printf("if ia  %c\n",star);
+    
+
+       return 0;*/
+      /*int age;
+       printf("enter age");
+       scanf("%d",&age);
+       printf("age is : %d",age );
+       return 0;
+
+       */ 
+
+      int a,b;
+      printf("enter a");
+      scanf("%d",&a);
+      printf("enter b");
+      scanf("%d",&b);
+
+      int sum=a+b;
+      printf("sum is :%d",sum);
+      return 0;
+
+} 
---
+++ Program2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    float radius;
+    printf("area of  radius");
+    scanf("%f",&radius);
+    printf("area of :%f",3.14 *radius*radius);
+    return 0;
+}
